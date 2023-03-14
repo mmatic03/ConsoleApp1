310dd5b1ea50e66aa85ce467fa9794d62a985e90
andelamj <116180366+andelamj@users.noreply.github.com>
2023-03-14 18:58:47 +0100

    fhghdfgv

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("Novo");
+            Console.WriteLine("hjzfjzf");
         }
     }
 }
